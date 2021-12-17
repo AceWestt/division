f335de7eb2d6ac5c801ff10cd09450c17162b839
AceWestt <asilbekkhamzaev@gmail.com>
2021-12-17 10:35:13 +0500

    final init

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(`${__dirname}/client`, 'build')));
 app.get(/^\/(?!api).*$/, function (req, res) {
 	res.sendFile(path.join(`${__dirname}/client`, 'build', 'index.html'));
 });
