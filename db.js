eabd6f6c48931093a05c8952a34b32f8641e3652
AceWestt <asilbekkhamzaev@gmail.com>
2022-05-09 22:47:31 +0400

    fix

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const username = 'doadmin';
-const password = 'v41ua0TckV6I5793';
-const dbName = 'admin';
-const host = 'private-dbaas-db-48635-4df82001.mongo.ondigitalocean.com';
+const username = "doadmin";
+const password = "v41ua0TckV6I5793";
+const dbName = "admin";
+const host = "private-dbaas-db-48635-4df82001.mongo.ondigitalocean.com";
 
 const mongodbProdUri = `mongodb+srv://${username}:${password}@${host}/${dbName}`;
 
 const options = {
-	ssl: true,
-	sslValidate: true,
-	sslCA: `${__dirname}/ca-certificate.crt`,
+  ssl: true,
+  sslValidate: true,
+  sslCA: `${__dirname}/ca-certificate.crt`,
 };
 
 const connectDB = async () => {
-	await mongoose.connect(process.env.MONGO_URI || (mongodbProdUri, options));
-	console.log('MongoDB connected.');
+  await mongoose.connect(process.env.MONGO_URI || mongodbProdUri, options);
+  console.log("MongoDB connected.");
 };
 
 module.exports = connectDB;
