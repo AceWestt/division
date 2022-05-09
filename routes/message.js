cc0349aa4a62952a343bd7e1613b2b37cd00a147
AceWestt <asilbekkhamzaev@gmail.com>
2022-05-09 22:21:43 +0400

    pixel api

--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const { getall, add, deleteMessage } = require('../controllers/message');
+const { getall, add, deleteMessage } = require("../controllers/message");
 
-const { protect } = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 
-router.route('/').get(protect, getall);
-router.route('/').post(add);
-router.route('/:id').delete(protect, deleteMessage);
+router.route("/").get(protect, getall);
+router.route("/").post(add);
+router.route("/:id").delete(protect, deleteMessage);
 
 module.exports = router;
